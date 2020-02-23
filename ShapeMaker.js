e3f6848ef2c08f5e2ff76f5041f6b6984c94e2c2
Junhao Liao <junhao@junhao.ca>
2020-02-23 14:38:15 -0500

    Use textnode instead of innertext as asked in the question. added comments

--- ShapeMaker.js
+++ ShapeMaker.js
@@ -3,23 +3,34 @@ function ShapeMaker() {
 }
 
 ShapeMaker.prototype = {
-    lastWasCircle: false,
     addSquare: function () {
-        this.lastWasCircle = false;
+
+        // every square should be in a new line: div is the best choice
         const square = document.createElement("div");
-        square.className = "square";
-        square.innerHTML = "SQUARE";
+        square.className = "square"; // easy to set style
+
+        // as specified by the question: no innerHTML/innerText permitted
+        const squareTextNode = document.createTextNode("SQUARE");
+        square.append(squareTextNode);
+
+        // append the shape to "body"
         const myBody = document.querySelector("body");
         myBody.append(square);
 
     },
     addCircle: function () {
 
+        // circles should follow the last circle: span is the best choice
         const circle = document.createElement("span");
         circle.className = "circle";
-        circle.innerHTML = "CIRCLE";
+
+        // as specified by the question: no innerHTML/innerText permitted
+        const circleTextNode = document.createTextNode("CIRCLE");
+        circle.append(circleTextNode);
+
+        // append the shape to "body"
         const myBody = document.querySelector("body");
         myBody.append(circle);
 
     }
-}
+};
--- shapes.js
+++ shapes.js
@@ -1,10 +1,12 @@
-
+// find the elements
 const  button_s = document.querySelector("#button_s");
 const button_c = document.querySelector("#button_c");
 
+// link the events with handler functions
 button_s.addEventListener("click",add_square_handler);
 button_c.addEventListener("click",add_circle_handler);
 
+// instantiate the maker class
 const shape = new ShapeMaker;
 
 function add_square_handler(e) {
